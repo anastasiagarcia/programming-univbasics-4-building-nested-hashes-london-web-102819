21810ec2f6ba898a44a9f9f44465dc3efc2cf2b0
Anastasia Pankina <siapankina@gmail.com>
2019-09-17 17:39:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ end
 
 def monopoly_with_third_tier
   monopoly=monopoly_with_second_tier();
-  monopoly[:railroads][:rent_in_dollars]={:one_piece_owned=> 25,:two_pieces_owned=> 50, :three_pieces_owned=> 100, :key=>{}};
+  monopoly[:railroads][:rent_in_dollars]={:one_piece_owned=> 25,:two_pieces_owned=> 50, :three_pieces_owned=> 100, :four_pieces_owned=> 200;
   monopoly[:railroads][:names]={:key1=>{},:key2=>{}, :key3=>{}, :key4=>{}};
   
   return monopoly;
