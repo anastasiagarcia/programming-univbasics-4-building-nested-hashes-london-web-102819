1c65407c8aef2df564519f2041462092b6ee39f4
Anastasia Pankina <siapankina@gmail.com>
2019-09-17 17:25:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ end
 
 def monopoly_with_third_tier
   monopoly=monopoly_with_second_tier();
-  monopoly[:rent_in_dollars]={};
+  monopoly[:railroads][:rent_in_dollars]={};
   
   return monopoly;
   # When you start writing the implementation for this method, copy the Hash
